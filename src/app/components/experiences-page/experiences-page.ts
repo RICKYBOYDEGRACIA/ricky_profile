615a985b878977c645f33b271cbd9221f0d31f00
Ricky Boy De Gracia <ricky@reach52.com>
2026-09-05 15:29:53 +0800

    experience page

--- src/app/components/experiences-page/experiences-page.ts
+++ src/app/components/experiences-page/experiences-page.ts
@@ -1,9 +1,62 @@
 import { Component } from '@angular/core';
+import personalData  from '../../data/personal-data.json'
+import { CommonModule } from '@angular/common';
 
+type Experience = {
+  basics?: [
+    email: string,
+    label: string,
+    location: [
+      city: string,
+      country: string,
+    ],
+    name: string,
+    profiles: [
+      {
+        network: string,
+        url: string,
+      }
+    ],
+    summary: string,
+  ],
+  header: string,
+  jobTitle: string,
+  highligths: string[],
+  yearStarted?: string,
+  yearEnded?: string,
+  combinedYear?: string,
+  setup: string,
+}
 @Component({
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   selector: 'app-experiences-page',
   styleUrl: './experiences-page.css',
   templateUrl: './experiences-page.html',
 })
-export class ExperiencesPage {}
+export class ExperiencesPage {
+
+  myData = personalData;
+  basics = this.myData.basics;
+  constructor(
+  ){
+    console.log(this.myData)
+  }
+
+  experiences : Experience[] = this.myData.experience.map((item) => ({
+    header: item.position
+    .trim()
+    .split(/\s+/)   
+    .slice(0,2) // Split by space(s)
+    .map(word => word.charAt(0)) // Take first letter of each word
+    .join('') // Combine into a single string ("RB")
+    .toUpperCase(),
+    jobTitle: item.position,
+    highligths: item.highlights,
+    yearStarted: item.startDate,
+    yearEnded: item.endDate,
+    combinedYear: `${item.startDate} - ${item.endDate}`,
+    setup: item.setup,
+  }))
+}
