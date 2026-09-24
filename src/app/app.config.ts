9685f306835d1df77e0fb2976e534531c49cf887
Ricky Boy De Gracia <ricksdegracia@gmail.com>
2026-09-24 12:55:54 +0800

    Concatenate web name to current page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
+import { TemplatePageTitleStrategy } from './title-strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,37 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./components/home-page/home-page').then(m => m.HomePage)  
+        loadComponent: () => import('./components/home-page/home-page').then(m => m.HomePage),
+        title: "Home" 
     },
     {
         path: 'sidebar',
-        loadComponent: () => import('./components/side-bar/side-bar').then(m => m.SideBar)  
+        loadComponent: () => import('./components/side-bar/side-bar').then(m => m.SideBar)
     },
     {
         path: 'certificates',
-        loadComponent: () => import('./components/certificates-page/certificates-page').then(m => m.CertificatesPage)
+        loadComponent: () => import('./components/certificates-page/certificates-page').then(m => m.CertificatesPage),
+        title: 'Certificates'
     },
     {
         path: 'projects',
-        loadComponent: () => import('./components/projects-page/projects-page').then(m => m.ProjectsPage)
+        loadComponent: () => import('./components/projects-page/projects-page').then(m => m.ProjectsPage),
+        title: 'Projects'
     },
     {
         path: 'gears',
-        loadComponent: () => import('./components/gears-page/gears-page').then(m => m.GearsPage)
+        loadComponent: () => import('./components/gears-page/gears-page').then(m => m.GearsPage),
+        title: 'Gears'
     },
     {
         path: 'blogs',
-        loadComponent: () => import('./components/blogs-page/blogs-page').then(m => m.BlogsPage)
+        loadComponent: () => import('./components/blogs-page/blogs-page').then(m => m.BlogsPage),
+        title: 'Blogs'
     },
     {
         path: 'experiences',
-        loadComponent: () => import('./components/experiences-page/experiences-page').then(m => m.ExperiencesPage)
+        loadComponent: () => import('./components/experiences-page/experiences-page').then(m => m.ExperiencesPage),
+        title: 'Experiences'
     }
 
 ];
